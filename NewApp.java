60cf7d4e2563f6e8d17c5aefc354466c7cefd54d
kamrul4950 <kamrulasad49@gmail.com>
2019-11-17 20:43:10 +0600

    New Mathod Add name AddSum

--- NewApp.java
+++ NewApp.java
@@ -1,5 +1,12 @@
 public class NewApp{
 	public static void main(String[] args) {
 		System.out.println("Git Bash Cammend is Awesome..");
+
+		System.out.println("Tow Digits Sumution Is :"+AddSum(4,6));
+	}
+
+
+	public static int AddSum(int a,int b){
+		return a+b;
 	}
 }
