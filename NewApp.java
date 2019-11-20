3caa52531f599dfbf250339b7309f639fcb55059
kamrul4950 <kamrulasad49@gmail.com>
2019-11-20 07:47:32 +0600

    Sum Method Create

--- NewApp.java
+++ NewApp.java
@@ -1,20 +1,18 @@
 import java.util.Scanner;
 public class NewApp{
-
-	public static void nameFun(String name){
-		System.out.println("Welcome " +name);
-	}
-
-
-	public static void main(String[] Args){
-		Scanner value = new Scanner(System.in);
-		System.out.print("Enter Your Name : ");
-			String name=value.next();
-			nameFun(name);
-		
-
-
-	}
-
 	
+	public static int sumutionFun(int a,int b){
+		int sum = a+b;
+		return sum;
+	}
+
+	public static void main(String[] args){
+		Scanner input =new Scanner(System.in);
+		System.out.print("Enter Two Integer Number : ");
+
+			int num1 = input.nextInt();
+			int num2 = input.nextInt();
+		System.out.println("The Sum is :" + sumutionFun(num1,num2));
+
+	}
 }
